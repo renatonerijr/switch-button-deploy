4c5baff51591addf89c15a9686dff5d07bb3e77c
Renato Neri Junior <renatonerijr@gmail.com>
2023-01-28 18:07:31 -0300

    feat(export): Export switch button

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+export * from './components/switch-button'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />,
 );
